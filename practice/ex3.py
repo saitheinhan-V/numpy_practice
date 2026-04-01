152f329b32aa1523e7b489e75d0ff9b5edacfc15
saitheinhan-V <saihlainhan1541997@gmail.com>
2026-04-01 12:14:00 +0630

    Array(Indexing,Slicing,View,Copy)

---
+++ practice/ex3.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5],dtype='S')
+print(arr)
+
+arr = np.array([1.1, 2.1, 3.1])
+
+newarr = arr.astype('i')
+
+print(newarr)
+print(newarr.dtype)
+
+arr = np.array([1, 0, 3])
+
+newarr = arr.astype(bool)
+
+print(newarr)
+print(newarr.dtype)
